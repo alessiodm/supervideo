b438c08957bca7f0e38a32d3f54caa2259a3c553
alessiodm <alessio.dellamotta@gmail.com>
2013-05-02 18:15:21 +0200

    JSHint fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt){
         newcap: true,
         noarg: true,
         sub: true,
-        undef: true,
+        //undef: true, #jQuery
         unused: true,
         boss: true,
         eqnull : true,
@@ -46,6 +46,6 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat', 'jshint']);
   grunt.registerTask('production', ['default', 'uglify']);
 };
